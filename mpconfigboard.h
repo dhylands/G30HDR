3d74ada365a2ca71be26d8d20f51e9f1a8bbf869
Dave Hylands <dhylands@gmail.com>
2016-01-08 09:58:43 -0800

    Enabled RTC

--- mpconfigboard.h
+++ mpconfigboard.h
@@ -8,7 +8,7 @@
 #define MICROPY_HW_HAS_LIS3DSH      (0)
 #define MICROPY_HW_HAS_LCD          (0)
 #define MICROPY_HW_ENABLE_RNG       (0)
-#define MICROPY_HW_ENABLE_RTC       (0)
+#define MICROPY_HW_ENABLE_RTC       (1)
 #define MICROPY_HW_ENABLE_TIMER     (1)
 #define MICROPY_HW_ENABLE_SERVO     (1)
 #define MICROPY_HW_ENABLE_DAC       (0)
